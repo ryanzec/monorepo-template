b83b39735656ec4aa25272087a4ea7da3fd70e5d
Ryan Zec <basire@gmail.com>
2022-09-05 09:28:10 -0400

    more files not causing the error

---
+++ packages/components/application-frame/tests/application-frame.spec.tsx
@@ -0,0 +1,78 @@
+import 'mocha';
+import { expect } from 'chai';
+import React from 'react';
+import sinon from 'sinon';
+
+import * as component from '$/components/application-frame/application-frame';
+import { ThemeName } from '$/types/theme';
+import { unitTestingUtils } from '$/utils/unit-testing';
+
+describe('application frame component', () => {
+  describe('internalOnLogout', () => {
+    it('works', () => {
+      const event = {
+        preventDefault: sinon.stub(),
+        stopPropagation: sinon.stub(),
+      };
+      const logout = sinon.stub();
+
+      component.internalOnLogout({
+        event: unitTestingUtils.castAnyAs<React.MouseEvent<HTMLButtonElement, MouseEvent>>(event),
+        logout,
+      });
+
+      expect(event.preventDefault.callCount).to.equal(1);
+      expect(event.preventDefault.getCall(0).args).to.deep.equal([]);
+      expect(event.stopPropagation.callCount).to.equal(1);
+      expect(event.stopPropagation.getCall(0).args).to.deep.equal([]);
+      expect(logout.callCount).to.equal(1);
+      expect(logout.getCall(0).args).to.deep.equal([]);
+    });
+  });
+
+  describe('internalOnToggleTheme', () => {
+    it('toggle to light', () => {
+      const event = {
+        preventDefault: sinon.stub(),
+        stopPropagation: sinon.stub(),
+      };
+      const theme = ThemeName.DARK;
+      const setTheme = sinon.stub();
+
+      component.internalOnToggleTheme({
+        event: unitTestingUtils.castAnyAs<React.MouseEvent<HTMLButtonElement, MouseEvent>>(event),
+        theme,
+        setTheme,
+      });
+
+      expect(event.preventDefault.callCount).to.equal(1);
+      expect(event.preventDefault.getCall(0).args).to.deep.equal([]);
+      expect(event.stopPropagation.callCount).to.equal(1);
+      expect(event.stopPropagation.getCall(0).args).to.deep.equal([]);
+      expect(setTheme.callCount).to.equal(1);
+      expect(setTheme.getCall(0).args).to.deep.equal([ThemeName.LIGHT]);
+    });
+
+    it('toggle to dark', () => {
+      const event = {
+        preventDefault: sinon.stub(),
+        stopPropagation: sinon.stub(),
+      };
+      const theme = ThemeName.LIGHT;
+      const setTheme = sinon.stub();
+
+      component.internalOnToggleTheme({
+        event: unitTestingUtils.castAnyAs<React.MouseEvent<HTMLButtonElement, MouseEvent>>(event),
+        theme,
+        setTheme,
+      });
+
+      expect(event.preventDefault.callCount).to.equal(1);
+      expect(event.preventDefault.getCall(0).args).to.deep.equal([]);
+      expect(event.stopPropagation.callCount).to.equal(1);
+      expect(event.stopPropagation.getCall(0).args).to.deep.equal([]);
+      expect(setTheme.callCount).to.equal(1);
+      expect(setTheme.getCall(0).args).to.deep.equal([ThemeName.DARK]);
+    });
+  });
+});
