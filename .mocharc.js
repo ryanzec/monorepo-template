4f96088909f7a3e9d48ea1b1ee95f8b2e5245fc5
Ryan Zec <basire@gmail.com>
2022-09-07 05:50:18 -0400

    mocha config update

--- .mocharc.js
+++ .mocharc.js
@@ -2,4 +2,5 @@ module.exports = {
   require: ['ts-node/register', 'tsconfig-paths/register'],
   spec: ['src/**/*.spec.ts', 'src/**/*.spec.tsx', 'packages/**/*.spec.ts', 'packages/**/*.spec.tsx'],
   file: ['./mocha-setup.ts'],
+  config: '.mocharc.js',
 };
